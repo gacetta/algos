859a7f3029b74fe44e5a22ffd912d3190a5b8366
gacetta <78240758+gacetta@users.noreply.github.com>
2022-11-13 09:56:10 -0800

    feat: added hobbyTracker.js

---
+++ closure/hobbyTracker.js
@@ -0,0 +1,36 @@
+ // function hobbyTracker
+ // input: arr
+ // creates a cache obj
+ // out: function
+
+// returned func
+// input: string hobby, integer numb of hours
+// when invoked - update cache.  add the hours to the corresponding property.
+// if no args, reset all values to 0, return 'tracker has been reset!'
+
+function hobbyTracker(arr) {
+  const cache = {};
+  arr.forEach((hobby) => {
+    cache[hobby] = 0;
+  })
+
+  return (hobby, hours) => {
+    if (hobby === undefined) {
+      for (const key in cache) {
+        cache[key] = 0;
+      }
+      return 'tracker has been reset!'
+    } else {
+      cache[hobby] ? cache[hobby] += hours : cache[hobby] = hours;
+      return cache;
+    }
+  }
+}
+
+const updateHobbies = hobbyTracker(['yoga', 'baking', 'piano']);
+updateHobbies('yoga', 2);
+updateHobbies('baking', 4);
+updateHobbies('yoga', 1);
+console.log(updateHobbies('piano', 2)); // --> { yoga: 3, baking: 4, piano: 2 }
+console.log(updateHobbies()); // --> 'tracker has been reset!'
+console.log(updateHobbies('baking', 1)); // --> { yoga: 0, baking: 1, piano: 0}
