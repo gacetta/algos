6b7843046ecd17766c3697f94e477c123a771f63
gacetta <78240758+gacetta@users.noreply.github.com>
2022-11-26 15:19:43 -0800

    feat: solved and added FCC solution

--- stringManipulation/converHTML.js
+++ stringManipulation/converHTML.js
@@ -4,8 +4,31 @@ in a string to their corresponding HTML entities.
 */
 
 function convertHTML(str) {
-  
+  if (/<>/.test(str)) return str.replaceAll("<>", '&lt;&gt;')
+  if (/'/.test(str)) return str.replaceAll("'", '&apos;')
+  if (/&/.test(str)) return str.replaceAll("&", '&amp;')
+  if (/</.test(str)) return str.replaceAll("<", '&lt;')
+  if (/>/.test(str)) return str.replaceAll(">", '&gt;')
+  if (/"/.test(str)) return str.replaceAll('"', '&quot;')
+  return str;
 }
+
+// from FCC
+// function convertHTML(str) {
+//   // Use Object Lookup to declare as many HTML entities as needed.
+//   const htmlEntities = {
+//     "&": "&amp;",
+//     "<": "&lt;",
+//     ">": "&gt;",
+//     '"': "&quot;",
+//     "'": "&apos;"
+//   };
+//   //Use map function to return a filtered str with all entities changed automatically.
+//   return str
+//     .split("")
+//     .map(entity => htmlEntities[entity] || entity)
+//     .join("");
+// }
 
 console.log(convertHTML("Dolce & Gabbana")) // Dolce &amp; Gabbana.
 console.log(convertHTML("Hamburgers < Pizza < Tacos")) // Hamburgers &lt; Pizza &lt; Tacos.
