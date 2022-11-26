19b5df90f37cd59f88a8c922fe60d4aa2149627e
gacetta <78240758+gacetta@users.noreply.github.com>
2022-11-26 09:56:36 -0800

    feat: created pigLatin challenge

---
+++ stringManipulation/pigLatin.js
@@ -0,0 +1,22 @@
+/* Pig Latin
+Pig Latin is a way of altering English Words. The rules are as follows:
+
+- If a word begins with a consonant, take the first consonant or consonant cluster, 
+move it to the end of the word, and add ay to it.
+
+- If a word begins with a vowel, just add way at the end.
+
+Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
+*/
+
+function translatePigLatin(str) {
+
+}
+
+console.log(translatePigLatin("california")) //aliforniacay.
+console.log(translatePigLatin("paragraphs")) //aragraphspay.
+console.log(translatePigLatin("glove")) //oveglay.
+console.log(translatePigLatin("algorithm")) //algorithmway.
+console.log(translatePigLatin("eight")) //eightway.
+console.log(translatePigLatin("schwartz")) //artzschway.
+console.log(translatePigLatin("rhythm")) //rhythmay.
