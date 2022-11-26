ced06773b25fba9deab6f877544bb3717d62e242
gacetta <78240758+gacetta@users.noreply.github.com>
2022-11-26 11:16:59 -0800

    feat: solved

--- stringManipulation/pigLatin.js
+++ stringManipulation/pigLatin.js
@@ -9,8 +9,26 @@ move it to the end of the word, and add ay to it.
 Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
 */
 
-function translatePigLatin(str) {
+//input: string - lowercase english word
+// output: string in piglatin
+// if first char is a vowel, add `way` to the end
+/// if first char is consonant, move first consonant or consonant cluster to the end and add `ay` to it.
 
+function translatePigLatin(str) {
+  const isConsonant = /[^aeiou]/;
+  const firstLetter = str[0];
+
+  // if first letter is vowel
+  if (!isConsonant.test(firstLetter)) return str + 'way';
+
+  // if first letter is consonant
+  for (let i = 0; i < str.length; i++) {
+    if (!isConsonant.test(str[i])) {
+      const endStr = str.slice(0, i) + 'ay';
+      return str.slice(i) + endStr;
+    }
+  }
+  return str + 'ay'
 }
 
 console.log(translatePigLatin("california")) //aliforniacay.
