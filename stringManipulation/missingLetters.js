b447f401cc3033d28d75370dd10412f74df93def
gacetta <78240758+gacetta@users.noreply.github.com>
2022-11-26 13:12:57 -0800

    feat: created missingLetters algo

---
+++ stringManipulation/missingLetters.js
@@ -0,0 +1,11 @@
+/* Missing letters
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+*/
+
+console.log(fearNotLetter("abce")) // d.
+console.log(fearNotLetter("abcdefghjklmno")) // i.
+console.log(fearNotLetter("stvwx")) // u.
+console.log(fearNotLetter("bcdf")) // e.
+console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz")) // undefined.
