607b8f1b17da996c584b650addf6babc3f4c1c09
gacetta <78240758+gacetta@users.noreply.github.com>
2022-11-29 09:05:12 -0800

    feat: created new sliding window algo

---
+++ slidingWindow/findLongestSubstring.js
@@ -0,0 +1,20 @@
+/* Find Longest Substring
+Write a function called findLongestSubstring, which 
+accepts a string and returns the length of the longest 
+substring with all distinct characters.
+
+Time complexity O(n)
+*/
+
+function findLongestSubstring(str) {
+
+}
+
+console.log(findLongestSubstring('')) // 0
+console.log(findLongestSubstring('rithmschool')) // 7
+console.log(findLongestSubstring('thisisawesome')) // 6
+console.log(findLongestSubstring('thecatinthehat')) // 7
+console.log(findLongestSubstring('bbbbbb')) // 1
+console.log(findLongestSubstring('longestsubstring')) // 8
+console.log(findLongestSubstring('thisishowwedoit')) // 6
+
