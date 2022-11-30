70569decfa5b20666fd59544525261a39de8c5da
gacetta <78240758+gacetta@users.noreply.github.com>
2022-11-30 08:12:14 -0800

    feat: solved

--- async/limitedInterval.js
+++ async/limitedInterval.js
@@ -1,16 +1,23 @@
-let dataReceived;
+/* limitedInterval
 
-function ajaxSimulate(id, callback) {
-  const database = ['Aaron', 'Barbara', 'Chris'];
-	setTimeout(()=>{
-    const element = database[id];
-    callback(element);
-  }, 0);
+Write a function called limitedInterval that accepts 
+as arguments in this order -
+
+callback function
+wait time in milliseconds
+limit time in milliseconds.
+
+limitedInterval should run the callback once every 
+wait milliseconds, up to limit milliseconds, and then stop.
+*/
+
+//input: callback, wait in ms, limit, in ms
+
+function limitedInterval(callback, wait, limit) {
+  for (let i = wait; i <= limit; i += wait) {
+    console.log(`setting timeout for ${i}ms`)
+    setTimeout(callback, i);
+  }
 }
 
-function storeData(data) {
-  dataReceived = data;
-  console.log(dataReceived);
-}
-
-ajaxSimulate(1, storeData);
+limitedInterval(() => console.log('repeating'), 100, 550);
