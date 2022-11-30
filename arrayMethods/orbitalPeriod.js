6ed00abc72779cf60a7ff03105922c30c7e8398b
gacetta <78240758+gacetta@users.noreply.github.com>
2022-11-30 11:05:22 -0800

    feat: solved

--- arrayMethods/orbitalPeriod.js
+++ arrayMethods/orbitalPeriod.js
@@ -20,9 +20,19 @@ The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 39
 function orbitalPeriod(arr) {
   const GM = 398600.4418;
   const earthRadius = 6367.4447;
-  return arr;
+
+  return arr.map((obj) => {
+    const name = obj.name;
+    const objAlt = obj.avgAlt;
+    const orbitRadius = earthRadius + objAlt;
+
+    const orbitalPeriod = Math.round(2*Math.PI * Math.sqrt(Math.pow(orbitRadius, 3) / GM));
+    return { name, orbitalPeriod };
+  })
 }
 
 
-console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}])) // [{name: "sputnik", orbitalPeriod: 86400}].
-console.log(orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}])) // [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
+console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}])) 
+// [{name: "sputnik", orbitalPeriod: 86400}].
+console.log(orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}])) 
+// [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
