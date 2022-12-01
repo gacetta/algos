be5057d3ff60eedee31b54f13cbaf8a5907efeba
gacetta <78240758+gacetta@users.noreply.github.com>
2022-11-30 19:11:41 -0800

    feat: solved (cheated with regex cause didn't have time)

--- regex/telephoneValidator.js
+++ regex/telephoneValidator.js
@@ -13,16 +13,17 @@ For this challenge you will be presented with a string such as 800-692-7753 or 8
 */
 
 function telephoneCheck(str) {
-  if (!((str.length >= 12 && str.length <=14) || str.length === 10)) {
-    return false;
-  }
-  const regexCheck = /somethingHere/
-  return regexCheck.test(str);
+  // if (!((str.length >= 12 && str.length <=14) || str.length === 10)) {
+  //   return false;
+  // }
+  const regexCheck1 = /^(1\s?)?\d{3}([-\s]?)\d{3}\2\d{4}$/
+  const regexCheck2 = /^(1\s?)?\(\d{3}\)\s?\d{3}[-\s]?\d{4}$/
+  return regexCheck1.test(str) ? true : regexCheck2.test(str);
 }
 
 
-console.log(telephoneCheck("555-555-5555")) // a boolean.
-console.log(telephoneCheck("1 555-555-5555")) // true.
+// console.log(telephoneCheck("555-555-5555")) // a boolean.
+// console.log(telephoneCheck("1 555-555-5555")) // true.
 // console.log(telephoneCheck("1 (555) 555-5555")) // true.
 // console.log(telephoneCheck("5555555555")) // true.
 // console.log(telephoneCheck("555-555-5555")) // true.
@@ -33,20 +34,20 @@ console.log(telephoneCheck("1 555-555-5555")) // true.
 // console.log(telephoneCheck("1 555)555-5555")) // false.
 // console.log(telephoneCheck("1 555 555 5555")) // true.
 // console.log(telephoneCheck("1 456 789 4444")) // true.
-// console.log(telephoneCheck("123**&!!asdf#")) // false.
-// console.log(telephoneCheck("55555555")) // false.
-// console.log(telephoneCheck("(6054756961)")) // false.
-// console.log(telephoneCheck("2 (757) 622-7382")) // false.
-// console.log(telephoneCheck("0 (757) 622-7382")) // false.
-// console.log(telephoneCheck("-1 (757) 622-7382")) // false.
-// console.log(telephoneCheck("2 757 622-7382")) // false.
-// console.log(telephoneCheck("10 (757) 622-7382")) // false.
-// console.log(telephoneCheck("27576227382")) // false.
-// console.log(telephoneCheck("(275)76227382")) // false.
-// console.log(telephoneCheck("2(757)6227382")) // false.
-// console.log(telephoneCheck("2(757)622-7382")) // false.
-// console.log(telephoneCheck("555)-555-5555")) // false.
-// console.log(telephoneCheck("(555-555-5555")) // false.
-// console.log(telephoneCheck("(555)5(55?)-5555")) // false.
-// console.log(telephoneCheck("55 55-55-555-5")) // false.
-// console.log(telephoneCheck("11 555-555-5555")) // false.
+console.log(telephoneCheck("123**&!!asdf#")) // false.
+console.log(telephoneCheck("55555555")) // false.
+console.log(telephoneCheck("(6054756961)")) // false.
+console.log(telephoneCheck("2 (757) 622-7382")) // false.
+console.log(telephoneCheck("0 (757) 622-7382")) // false.
+console.log(telephoneCheck("-1 (757) 622-7382")) // false.
+console.log(telephoneCheck("2 757 622-7382")) // false.
+console.log(telephoneCheck("10 (757) 622-7382")) // false.
+console.log(telephoneCheck("27576227382")) // false.
+console.log(telephoneCheck("(275)76227382")) // false.
+console.log(telephoneCheck("2(757)6227382")) // false.
+console.log(telephoneCheck("2(757)622-7382")) // false.
+console.log(telephoneCheck("555)-555-5555")) // false.
+console.log(telephoneCheck("(555-555-5555")) // false.
+console.log(telephoneCheck("(555)5(55?)-5555")) // false.
+console.log(telephoneCheck("55 55-55-555-5")) // false.
+console.log(telephoneCheck("11 555-555-5555")) // false.
